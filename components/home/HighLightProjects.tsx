5ac07d4a37fd08d7ee3ac6eb648ab2210a10d4fe
NooobtimeX <nooobtimex@gmail.com>
2025-01-03 17:33:07 +0700

    high light

--- components/home/HighLightProjects.tsx
+++ components/home/HighLightProjects.tsx
@@ -2,18 +2,20 @@
 
 import Link from "next/link";
 import QRCodeProject from "./projects/QRCodeProject";
+import Combomaker from "./projects/Combomaker";
+import HoyoSmash from "./projects/HoyoSmash";
 
 export default function Project() {
   return (
     <div className="min-h-96 bg-neutral-900 py-4">
-      <h2 className="py-2 text-center text-4xl font-bold text-red-600">
-        Highlight Project
-      </h2>
+      <h2 className="py-2 text-center text-4xl font-bold">Highlight Project</h2>
       <QRCodeProject />
+      <Combomaker />
+      <HoyoSmash />
       <div className="mt-6 flex items-center justify-center">
         <Link href="/projects">
-          <button className="rounded-xl border border-red-600 px-6 py-3 text-red-600 transition duration-300 hover:bg-red-600 hover:text-white">
-            More Projects
+          <button className="border border-red-600 px-6 py-3 text-red-600 transition duration-300 hover:bg-red-600 hover:text-white">
+            MORE PROJECTS
           </button>
         </Link>
       </div>
---
+++ components/home/projects/HoyoSmash.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import Link from "next/link";
+
+export default function HoyoSmash() {
+  const skills = [
+    {
+      name: "NEXT JS",
+      src: "https://firebasestorage.googleapis.com/v0/b/portfolio-84dbc.firebasestorage.app/o/skill_logo%2Fnextjs.ico?alt=media&token=1bc6718b-bc03-4fac-8e5d-f07ed159f209",
+    },
+    {
+      src: "https://firebasestorage.googleapis.com/v0/b/portfolio-84dbc.firebasestorage.app/o/skill_logo%2Ftailwindcss.png?alt=media&token=9c897653-80bb-444f-b19f-9f938db2d303",
+      name: "Tailwind CSS",
+    },
+    {
+      src: "https://firebasestorage.googleapis.com/v0/b/portfolio-84dbc.firebasestorage.app/o/skill_logo%2Fjavascript.png?alt=media&token=c3cb89d0-81c7-48a4-9f78-6e90e99eac78",
+      name: "Javascript",
+    },
+    {
+      src: "https://firebasestorage.googleapis.com/v0/b/portfolio-84dbc.firebasestorage.app/o/skill_logo%2Ftypescript.ico?alt=media&token=9bcf541d-5e0a-469c-b32e-29c773fff098",
+      name: "Typescript",
+    },
+    {
+      src: "https://firebasestorage.googleapis.com/v0/b/portfolio-84dbc.firebasestorage.app/o/skill_logo%2Ffirebase.png?alt=media&token=e50b5c7b-10d4-4036-be9d-9fae6739bef8",
+      name: "FIREBASE",
+    },
+  ];
+  return (
+    <div className="p-2">
+      {/* HoyoSmash */}
+      <div className="mx-auto grid max-w-7xl grid-cols-1 gap-4 lg:grid-cols-2">
+        <div className="m-auto">
+          <div className="group relative">
+            <Link
+              href="https://hoyosmash.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/portfolio-84dbc.firebasestorage.app/o/project_banner%2Fhoyosmash.png?alt=media&token=b38f2ff5-c441-4e3b-866a-a4ab32b6b903"
+                alt="HoyoSmash"
+                className="mx-auto max-h-80"
+              />
+              <label className="absolute inset-0 flex items-center justify-center rounded-xl bg-black bg-opacity-70 text-lg font-bold text-white opacity-0 transition-opacity duration-300 hover:cursor-pointer group-hover:opacity-100">
+                HoyoSmash
+              </label>
+            </Link>
+          </div>
+        </div>
+
+        <div className="my-auto p-2">
+          <h3 className="text-center text-3xl font-bold">HoyoSmash</h3>
+          <div>
+            <p className="text-center text-lg">Skills</p>
+            <div className="flex flex-wrap justify-center gap-3">
+              {skills.map((skill, index) => (
+                <div
+                  key={index}
+                  className="flex h-16 w-16 transform flex-col items-center justify-center rounded-lg bg-neutral-800 shadow-lg transition-transform duration-300 hover:scale-105 hover:bg-red-600 md:h-20 md:w-20 xl:h-24 xl:w-24"
+                >
+                  <img
+                    src={skill.src}
+                    width={50}
+                    height={50}
+                    alt={skill.name}
+                    className="rounded-none"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
