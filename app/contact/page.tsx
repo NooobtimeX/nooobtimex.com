81bae282cf89b0e191a1fececf2cb64d18e2b174
NooobtimeX <nooobtimex@gmail.com>
2025-01-04 10:21:50 +0700

    Recommend way to contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -16,32 +16,47 @@ export default function Contact() {
       title: "Facebook",
       detail: "Woongsaphat Puangsorn",
       link: "https://www.facebook.com/xnooobtime",
+      recommend: true,
     },
     {
       title: "Discord",
       detail: "nooobtimex",
       link: "https://discord.com",
+      recommend: true,
     },
   ];
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center p-4">
       <Head>
         <link rel="canonical" href="https://nooobtimex.me/contact" />
       </Head>
       <div className="w-full max-w-4xl px-4">
+        <h1>CONTACT</h1>
         <h2 className="mb-4 text-2xl font-bold">Get in touch</h2>
         <p className="mb-8">Wongsaphat Puangsorn</p>
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
           {contactDetails.map((detail, index) => (
             <div
               key={index}
-              className="my-auto gap-4 rounded-xl bg-neutral-800 px-4 py-10 shadow-md"
+              className="my-auto h-full gap-4 rounded-xl bg-neutral-800 px-4 py-10 shadow-md"
             >
-              <h3 className="text-lg font-semibold">{detail.title}</h3>
-              <a href={detail.link} className="text-red-600 hover:underline">
+              <h3 className="text-lg font-semibold text-red-600">
+                {detail.title}
+              </h3>
+              <a
+                href={detail.link}
+                className="hover:underline"
+                aria-label={`Contact via ${detail.title}`}
+              >
                 {detail.detail}
               </a>
+              {detail.recommend && (
+                <div className="mt-2 flex items-center">
+                  <p className="text-sm text-red-500">Recommended</p>
+                  <span className="ml-1 text-red-600">*</span>
+                </div>
+              )}
             </div>
           ))}
         </div>
