a44c46e37741e197215e31b8a98b6ccbd7f065ae
NooobtimeX <nooobtimex@gmail.com>
2025-01-07 19:42:59 +0700

    posts in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,30 +1,42 @@
+import { getAllPosts } from "@/lib/api";
 import { MetadataRoute } from "next";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+const domain = "https://nooobtimex.me";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = getAllPosts();
+  const postUrls = posts.map((post) => ({
+    url: `${domain}/posts/${post.slug}`,
+    lastModified: new Date(post.dateModified),
+    changeFrequency: "monthly" as const,
+    priority: 0.5,
+  }));
+
   return [
     {
-      url: "https://nooobtimex.me",
+      url: `${domain}`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: "https://nooobtimex.me/skills",
+      url: `${domain}/skills`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://nooobtimex.me/projects",
+      url: `${domain}/projects`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://nooobtimex.me/contact",
+      url: `${domain}/contact`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
+    ...postUrls,
   ];
 }
